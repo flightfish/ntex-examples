5096e7d09eb6fc5f0a5b36f450bbc469850f72fc
yh_win <yh_win@qq.com>
2022-07-28 10:03:38 +0800

    test ntex

--- template_yarte/src/main.rs
+++ template_yarte/src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default()) // enable logger
             .service(index)
     })
-    .bind("192.168.1.45:8080")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
