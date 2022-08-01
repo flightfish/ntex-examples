4f7c9d3620e5c2e9151a22991db936abc1fd9fbf
yh_win <yh_win@qq.com>
2022-08-01 12:02:16 +0800

    tmp

--- diesel/src/main.rs
+++ diesel/src/main.rs
@@ -7,7 +7,7 @@
 extern crate diesel;
 
 use diesel::prelude::*;
-use diesel::dsl::{self, ConnectionManager};
+use diesel::r2d2::{self, ConnectionManager};
 use ntex::web::{self, middleware, App, Error, HttpResponse};
 use uuid::Uuid;
 
@@ -15,7 +15,7 @@ mod actions;
 mod models;
 mod schema;
 
-type DbPool = dsl::Pool<ConnectionManager<SqliteConnection>>;
+type DbPool = r2d2::Pool<ConnectionManager<PostgresConnection>>;
 /// Finds user by UID.
 #[web::get("/user/{user_id}")]
 async fn get_user(
@@ -59,7 +59,7 @@ async fn main() -> std::io::Result<()> {
 
     // set up database connection pool
     let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    let manager = ConnectionManager::<PostgresConnection>::new(connspec);
     let pool = dsl::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
